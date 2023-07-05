7877a11bafdffa37c4b361070d6de3b256c67c76
Darius Ionatan Pop <Darius.Pop1@ibm.com>
2023-07-05 20:20:23 +0300

    bare app done

--- App.js
+++ App.js
@@ -1,20 +1,40 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, SafeAreaView } from "react-native";
+import BouncyCheckbox from "react-native-bouncy-checkbox";
+import * as Haptics from "expo-haptics";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <Text style={styles.header}>Urma Habits</Text>
+      <BouncyCheckbox
+        onPress={(isChecked) => {
+          Haptics.impactAsync(Haptics.ImpactFeedbackStyle.Heavy);
+        }}
+        text="Talk 5min to camera"
+      />
+      <BouncyCheckbox
+        onPress={(isChecked) => {
+          Haptics.impactAsync(Haptics.ImpactFeedbackStyle.Heavy);
+        }}
+        text="5min willpower training"
+        fillColor="red"
+      />
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    margin: 20,
+    gap: 10,
+  },
+  header: {
+    paddingTop: 30,
+    paddingBottom: 25,
+    fontSize: 40,
+    letterSpacing: 1.5,
   },
 });
